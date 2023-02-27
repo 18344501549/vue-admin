71a9575ed3d22479c1a7f2a17f47f92573549deb
18344501549 <91644061+18344501549@users.noreply.github.com>
2023-02-27 17:41:25 +0800

    搭建框架
    完成简单搭建

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue';
+// import './style.css';
+import router, { setupRouter } from './router/index';
+import App from './App.vue';
+
+async function bootstrap() {
+    const app = createApp(App);
+    setupRouter(app);
+    // 处理好路由再挂载页面
+    await router.isReady();
+    app.mount('#app');
+};
+
+bootstrap();
